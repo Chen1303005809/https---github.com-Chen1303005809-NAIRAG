59abe6625c1270eb5045da83092ed60a1c444657
eurus <1303005809@qq.com>
2026-04-24 16:29:16 +0800

    add. drop script

---
+++ scripts/one_off/drop.py
@@ -0,0 +1,12 @@
+from pymilvus import connections, Collection, utility
+
+# 连接 Milvus（按你的实际地址修改）
+connections.connect(alias="default", host="127.0.0.1", port="19530")
+
+collection_name = "TEST"
+
+if utility.has_collection(collection_name):
+    Collection(collection_name).drop()
+    print(f"已删除向量库: {collection_name}")
+else:
+    print(f"向量库不存在: {collection_name}")
